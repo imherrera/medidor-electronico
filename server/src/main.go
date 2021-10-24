60b35b68062e34159e6fd300e1b2113cbb7a8ac5
Juan Herrera <juanhr454@gmail.com>
2021-10-24 14:58:34 -0300

    s

--- server/src/main.go
+++ server/src/main.go
@@ -5,16 +5,50 @@ import (
 	"html"
 	"log"
 	"net/http"
+	"time"
+
+	"github.com/go-redis/redis/v8"
+	"github.com/gorilla/mux"
 )
 
+type EnergyConsumption struct {
+	MeterID        string    `json:"meter_id"`
+	Date           time.Time `json:"date"`
+	ActualWattHour string    `json:"actual_watt_hour"`
+	ExpectWattHour string    `json:"expect_watt_hour"`
+}
+
+// Global reference to redis database client
+var rdb *redis.Client
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	router := mux.NewRouter()
+	rdb = redis.NewClient(&redis.Options{
+		Addr:     "localhost:1300",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 	})
 
-	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hi")
 	})
 
-	log.Fatal(http.ListenAndServe(":8081", nil))
+	// Init http server
+	err := http.ListenAndServe(":8080", router)
+	if err != nil {
+		log.Fatalln("Server Error: ", err)
+	}
+}
+
+func putEnergyConsumption(w http.ResponseWriter, r *http.Request) {
+	meterId := mux.Vars(r)["mid"]
+
+}
+
+func getEnergyConsumption(w http.ResponseWriter, r *http.Request) {
+
 }
