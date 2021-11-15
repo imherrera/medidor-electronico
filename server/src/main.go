1dd3138f87d5e66832f76a4c758d2fcb379e3e6f
Juan Herrera <juanhr454@gmail.com>
2021-11-15 10:26:54 -0300

    add arduino subrouter endpoint

--- server/src/main.go
+++ server/src/main.go
@@ -11,6 +11,9 @@ import (
 	"github.com/gorilla/mux"
 )
 
+/**
+ * Middleware de utilizad que añade el header json
+**/
 func jsonMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
 		rw.Header().Add("Content-Type", "application/json")
@@ -20,7 +23,8 @@ func jsonMiddleware(next http.Handler) http.Handler {
 
 /**
  * Middleware que interceptara los pedidos a este servidor
- * y validara los tokens de autorizacion, en caso de invalidez el pedido sera rechazado
+ * y validara los tokens de autorizacion obtenidos en el login de usuario,
+ * en caso de invalidez el pedido sera rechazado
 **/
 func tokenMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
@@ -39,27 +43,50 @@ func tokenMiddleware(next http.Handler) http.Handler {
 	})
 }
 
+/**
+ * Punto de entrada del programa
+**/
 func main() {
 	router := mux.NewRouter()
 	router.Use(jsonMiddleware)
 
+	/**
+	 * Rutas para la recoleccion de metricas de uso
+	**/
+	arduino := router.Methods("POST").Subrouter()
+	arduino.HandleFunc("/report/watt-hour", consumptionHandler).Methods("GET", "POST")
+
+	/**
+	 * Rutas para la autentificacion de cliente
+	**/
 	auth := router.Methods("POST").Subrouter()
 	auth.HandleFunc("/login", loginHandler).Methods("POST")
 
+	/**
+	 * Rutas para conexion del cliente web
+	**/
 	app := router.Methods("GET", "POST", "OPTIONS").Subrouter()
-	app.HandleFunc("/usage", consumptionHandler).Methods("GET", "POST")
 	app.HandleFunc("/usage/resume/{uci}", consumptionResumeHandler).Methods("GET")
 	app.Use(tokenMiddleware)
 
+	/**
+	 * Setup global de headers y CORS
+	**/
 	headers := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"})
 	origins := handlers.AllowedOrigins([]string{"*"}) // <- deberiamos aceptar solo al host de nuestra pagina web
 	methods := handlers.AllowedMethods([]string{"GET", "POST", "OPTIONS"})
 
+	/**
+	 * Utilizamos puerto definido por las variables de entorno
+	**/
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
 
+	/**
+	 * Iniciamos el servidor HTTP con un logger para debug
+	**/
 	err := http.ListenAndServe(":"+port, handlers.CombinedLoggingHandler(os.Stderr, handlers.CORS(headers, origins, methods)(router)))
 	if err != nil {
 		log.Fatalln("Server Error: ", err)
