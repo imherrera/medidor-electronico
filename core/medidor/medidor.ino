80f727654cbb18595f57fd3c1a55a8fe07a62475
Juan Herrera <juanhr454@gmail.com>
2021-11-15 10:26:20 -0300

    add http client and json serialization

--- core/medidor/medidor.ino
+++ core/medidor/medidor.ino
@@ -1,20 +1,146 @@
+#include <ArduinoHttpClient.h>
+#include <ArduinoJson.h>
+#include <Ethernet.h>
 #include <EmonLib.h>
-#include <Server.h>
+#include <SPI.h>
 
-const int S = 2;
-const EnergyMonitor energyMonitor;
-// the setup function runs once when you press reset or power the board
+// Enter a MAC address for your controller below.
+// Newer Ethernet shields have a MAC address printed on a sticker on the shield
+byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
+
+// if you don't want to use DNS (and reduce your sketch size)
+// use the numeric IP instead of the name for the server:
+IPAddress server(192, 168, 0, 12); // numeric IP for Google (no DNS)
+//char server[] = "192.168.0.12";    // name address for Google (using DNS)
+
+// Set the static IP address to use if the DHCP fails to assign
+IPAddress ip(192, 168, 0, 177);
+IPAddress myDns(192, 168, 0, 1);
+
+// Initialize the Ethernet client library
+// with the IP address and port of the server
+// that you want to connect to (port 80 is default for HTTP):
+EthernetClient client;
+
+// Variables to measure the speed
+unsigned long beginMicros, endMicros;
+unsigned long byteCount = 0;
+bool printWebData = true; // set to false for better speed measurement
+
+/**
+ * Codigo a ejecutarse al inicio
+ */
 void setup()
 {
-  // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
+
+  // start the Ethernet connection:
+  Serial.println("Initialize Ethernet with DHCP:");
+  if (Ethernet.begin(mac) == 0)
+  {
+    Serial.println("Failed to configure Ethernet using DHCP");
+    // Check for Ethernet hardware present
+    if (Ethernet.hardwareStatus() == EthernetNoHardware)
+    {
+      Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
+      while (true)
+      {
+        delay(1); // do nothing, no point running without Ethernet hardware
+      }
+    }
+    if (Ethernet.linkStatus() == LinkOFF)
+    {
+      Serial.println("Ethernet cable is not connected.");
+    }
+    // try to congifure using IP address instead of DHCP:
+    Ethernet.begin(mac, ip, myDns);
+  }
+  else
+  {
+    Serial.print("  DHCP assigned IP ");
+    Serial.println(Ethernet.localIP());
+  }
+  // give the Ethernet shield a second to initialize:
+  delay(1000);
+  Serial.print("connecting to ");
+  Serial.print(server);
+  Serial.println("...");
+
+  // if you get a connection, report back via serial:
+  if (client.connect(server, 8080))
+  {
+    Serial.print("connected to ");
+    Serial.println(client.remoteIP());
+    // Make a HTTP request:
+    client.println("GET /search?q=arduino HTTP/1.1");
+    client.println("Host: www.google.com");
+    client.println("Connection: close");
+    client.println();
+  }
+  else
+  {
+    // if you didn't get a connection to the server:
+    Serial.println("connection failed");
+  }
+  beginMicros = micros();
 }
 
-// the loop function runs over and over again forever
+/**
+ * Loop a ejecutarse indefinidamente luego de setup
+ */
 void loop()
 {
+  // if there are incoming bytes available
+  // from the server, read them and print them:
+  int len = client.available();
+  if (len > 0)
+  {
+    byte buffer[80];
+    if (len > 80)
+      len = 80;
+    client.read(buffer, len);
+    if (printWebData)
+    {
+      Serial.write(buffer, len); // show in the serial monitor (slows some boards)
+    }
+    byteCount = byteCount + len;
+  }
+
+  // if the server's disconnected, stop the client:
+  if (!client.connected())
+  {
+    endMicros = micros();
+    Serial.println();
+    Serial.println("disconnecting.");
+    client.stop();
+    Serial.print("Received ");
+    Serial.print(byteCount);
+    Serial.print(" bytes in ");
+    float seconds = (float)(endMicros - beginMicros) / 1000000.0;
+    Serial.print(seconds, 4);
+    float rate = (float)byteCount / seconds / 1000.0;
+    Serial.print(", rate = ");
+    Serial.print(rate);
+    Serial.print(" kbytes/second");
+    Serial.println();
+
+    // do nothing forevermore:
+    while (true)
+    {
+      delay(1);
+    }
+  }
+
+  DynamicJsonDocument doc(1024);
+  doc["sensor"] = "gps";
+  doc["time"] = 1351824120;
+  doc["data"][0] = 48.756080;
+  doc["data"][1] = 2.302038;
+  serializeJson(doc, Serial);
+
   digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
-  delay(1000);                         // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);      // turn the LED off by making the voltage LOW
-  delay(5000);                         // wait for a second
+  delay(1000);                     // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);  // turn the LED off by making the voltage LOW
+  delay(5000);                     // wait for a second
 }
