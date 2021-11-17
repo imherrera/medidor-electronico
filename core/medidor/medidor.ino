dfbae01aeaf1cee286132b5580fd705e370522e1
Juan Herrera <juanhr454@gmail.com>
2021-11-17 16:17:08 -0300

    fix margen of error by adjusting current variable

--- core/medidor/medidor.ino
+++ core/medidor/medidor.ino
@@ -10,6 +10,13 @@
 const double lineVoltage = 220.0; // voltaje de lineas de py
 
 /**
+ * Variables de programa
+ */
+unsigned long timeSinceLastReport = 0; // Tiempo del ultimo envio al servidor
+double ampSampleSum = 0;               // Acumulador de muestreos de amperaje
+int samplesTaken = 0;                  // Cantidad de muestras acumuladas de amperaje
+
+/**
  * Conexion a nuestro shield de internet
  */
 EthernetClient ethernet;
@@ -20,93 +27,85 @@ EthernetClient ethernet;
 HttpClient client = HttpClient(ethernet, server, port);
 
 /**
- *  Instancia de libreria para medir el consumo de potencia
+ *  Instancia de libreria para medir el consumo
  */
-EnergyMonitor emon1; // Create an instance
+EnergyMonitor emon1;
 
 /**
  * Codigo a ejecutarse al inicio
  */
 void setup()
 {
-  // Conectamos el puerto serial para debbugin
-  Serial.begin(9600);
-  // Seteamos el pin del sensor y voltaje de circuito a medir (220v paraguay)
-  emon1.current(1, 80); // Current: input pin, calibration.
-
+  // Check si la configuracion de DHCP fallo
   if (Ethernet.begin(mac) == 0)
   {
     Serial.println(F("Fallo al configurar Ethernet usando DHCP"));
-    // Check for Ethernet hardware present
-    if (Ethernet.hardwareStatus() == EthernetNoHardware)
+    // Check si tenemos el hardware
+    while (Ethernet.hardwareStatus() == EthernetNoHardware)
     {
       Serial.println(F("El ethernet shield no esta conectado!"));
-      while (true)
-      {
-        delay(1);
-      }
+      delay(1000);
     }
-    if (Ethernet.linkStatus() == LinkOFF)
+    // Check si el cable esta conectado
+    while (Ethernet.linkStatus() == LinkOFF)
+    {
       Serial.println(F("El cable de lan no esta conectado!"));
+      delay(1000);
+    }
   }
-  else
-  {
-    Serial.print(F("  DHCP asigno IP "));
-    Serial.println(Ethernet.localIP());
-  }
+  // Seteamos el pin del sensor de amperaje
+  emon1.current(1, 96.05);
   // esperamos a que el ethernet shield inicialice
   delay(1000);
-
-  Serial.println(F("Conectando al servidor..."));
 }
-
-/**
- * Tiempo del ultimo envio al servidor
- */
-unsigned long timeSinceLastReport = 0;
-
-double amps = 0;
-long samples = 0;
 
 /**
  * Loop a ejecutarse indefinidamente luego de setup
  */
 void loop()
 {
-  amps = amps + emon1.calcIrms(4000);
-  samples = samples + 1;
-  // Check si paso 5 segundos del ultimo envio
+  /**
+   * Acumulamos los muestreos de amperaje por 5 segundos y aumentamos nuestro contador de muestreo
+   */
+  ampSampleSum += emon1.calcIrms(1);
+  samplesTaken += 1;
+
+  /**
+   * Enviamos un reporte en intervalos de 5 seg
+   * 
+   * Check si pasaron 5 segundos del ultimo envio.
+   */
   if ((millis() - timeSinceLastReport) >= 5000)
   {
-    double mamps = (amps / samples);
-    double watts = (mamps * lineVoltage);
-    Serial.print("Samples: ");
-    Serial.println(samples);
-    Serial.print("Amps: ");
-    Serial.println(mamps);
-    Serial.print("Watts: ");
-    Serial.println(watts);
     /**
-   * Preparamos el reporte de consumo
-   */
+     * Dividimos la suma de muestreo por la cantidad de muestras tomadas para conseguir un 
+     * valor mas consistente de medida.
+     * 
+     * Calculamos watts consumidos asumiendo una constante de 220v
+     */
+    double ampsMeasured = ampSampleSum / samplesTaken;
+    double wattsMeasured = ampsMeasured * lineVoltage;
+
+    /**
+     * Preparamos el reporte de consumo
+     */
     const int capacity = JSON_OBJECT_SIZE(4);
     StaticJsonDocument<capacity> doc;
     doc["meter_id"] = uniqueDeviceId;
     doc["date"] = "2021-11-16T12:59:20.0268798-03:00"; // <- esta fecha se remplaza en el lado del servidor, esta aca hasta que agreguemos algun dispositivo para el tiempo
-    doc["watt_hour"] = watts;
-    doc["amps_hour"] = mamps;
+    doc["watt_hour"] = wattsMeasured;
+    doc["amps_hour"] = ampsMeasured;
 
     /**
-   * Serializamos a un string JSON
-   */
-    size_t reportLength = measureJson(doc) + 1; // + 1 para el incluir el terminador
+     * Serializamos a un string JSON
+     */
+    size_t reportLength = measureJson(doc) + 1; // + 1 para el incluir el terminador nulo
     char report[reportLength];
     serializeJson(doc, report, reportLength);
 
     /**
-    * Enviamos el reporte al servidor 
-    */
-    Serial.println("Sending report...");
+     * Enviamos el reporte al servidor en formato json
+     */
     client.beginRequest();
     client.post(reportPath);
     client.sendHeader("Authorization", uniqueDeviceSecret);
@@ -116,9 +115,12 @@ void loop()
     client.print(report);
     client.endRequest();
 
-    // Guardamos el ultimo tiempo de envio
+    /**
+     * Guardamos el ultimo tiempo de envio y reseteamos nuestras variables
+     * de muestreo 
+     */
     timeSinceLastReport = millis();
-    amps = 0;
-    samples = 0;
+    ampSampleSum = 0;
+    samplesTaken = 0;
   }
 }
